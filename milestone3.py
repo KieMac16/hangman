dcd4e566a7631fe732b7db403aea9fb79aa2a432
Kieran MacHugh <kieranmachugh@hotmail.co.uk>
2023-09-23 16:43:19 +0100

    Updated two files

--- milestone3.py
+++ milestone3.py
@@ -1,21 +1,21 @@
 import random
 
-word_list = ['banana','apple','pineapple','strawberry','grape']
-word = random.choice(word_list)
+my_word_list = ['banana','apple','pineapple','strawberry','grape']
+random_word = random.choice(my_word_list)
 
-def check_guess(guess):
-    if guess in word:
-        print(f"Good guess! {guess} is in the word")
+def check_user_guess(user_guess):
+    if user_guess in random_word:
+        print(f"Good guess! {user_guess} is in the word")
     else:
         print(f"Sorry, guess is not in the word. Try again")
 
 def ask_for_input():
     while True:
-        guess = input("Guess a letter: ")
-        if guess.isalpha and len(guess) == 1:
+        user_guess = input("Guess a letter: ")
+        if user_guess.isalpha and len(user_guess) == 1:
             break
         else:
             print("Invalid letter. Please enter a single alphabetical character")
-    check_guess(guess)
+    check_user_guess(user_guess)
 
 ask_for_input()
