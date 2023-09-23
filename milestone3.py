22a468e6c84c87700ecd6c56ce7e42684a0360ed
Kieran MacHugh <kieranmachugh@hotmail.co.uk>
2023-09-23 16:27:33 +0100

    Updated 3

--- milestone3.py
+++ milestone3.py
@@ -1,7 +1,21 @@
-while True:
-    guess = input("Guess a letter: ")
-    if guess.isalpha and len(guess) == 1:
-        print("Good guess!")
-        break
+import random
+
+word_list = ['banana','apple','pineapple','strawberry','grape']
+word = random.choice(word_list)
+
+def check_guess(guess):
+    if guess in word:
+        print(f"Good guess! {guess} is in the word")
     else:
-        print("Invalid letter. Please enter a single alphabetical character")
+        print(f"Sorry, guess is not in the word. Try again")
+
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter: ")
+        if guess.isalpha and len(guess) == 1:
+            break
+        else:
+            print("Invalid letter. Please enter a single alphabetical character")
+    check_guess(guess)
+
+ask_for_input()
